0da430f4344eea0dfe39e397c53d58a96014e7a7
jhy950210 <jhyy1995210@gmail.com>
2021-09-04 11:48:37 +0900

    반환타입 학습

--- data-jpa/src/test/java/com/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/com/study/datajpa/repository/MemberRepositoryTest.java
@@ -7,16 +7,19 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Rollback(false)
 class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
@@ -93,6 +96,18 @@ class MemberRepositoryTest {
         for (Member member : members) {
             System.out.println("member = " + member);
         }
+    }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> aaa = memberRepository.findListByUsername("AAA");
+        Member aaa1 = memberRepository.findMemberByUsername("AAA");
+        Optional<Member> aaa2 = memberRepository.findOptinalByUsername("AAA");
     }
 
 }
